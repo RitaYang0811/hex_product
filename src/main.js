922d87305e63d6b2f4f26e5a5d5ea2ab55536fc6
RitaYang <ritayang0811@gmail.com>
2024-01-26 03:20:18 +0800

    feat- admin後台商品取得和刪除

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+// import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import axios from 'axios'
 const app = createApp(App)
 
--- src/views/ProductsAdminView.vue
+++ src/views/ProductsAdminView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <div class="text-end mt-4">
-      <button class="btn btn-primary" @click="getProducts">建立新的產品</button>
+      <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productModal">
+        建立新的產品
+      </button>
     </div>
     <table class="table mt-4">
       <thead>
@@ -27,8 +29,25 @@
             </td>
             <td>
               <div class="btn-group">
-                <button type="button" class="btn btn-outline-primary btn-sm">編輯</button>
-                <button type="button" class="btn btn-outline-danger btn-sm">刪除</button>
+                <!-- 尚未確定編輯modal id -->
+                <button
+                  type="button"
+                  class="btn btn-outline-primary btn-sm"
+                  data-bs-toggle="modal"
+                  data-bs-target="#editModal"
+                  @click="editProduct(item, item.id)"
+                >
+                  編輯
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-outline-danger btn-sm"
+                  data-bs-toggle="modal"
+                  data-bs-target="#delProductModal"
+                  @click="dataId = item.id"
+                >
+                  刪除
+                </button>
               </div>
             </td>
           </tr>
@@ -36,7 +55,7 @@
       </tbody>
     </table>
   </div>
-  <!-- Modal -->
+  <!-- add Modal -->
   <div
     id="productModal"
     ref="productModal"
@@ -78,17 +97,35 @@
             <div class="col-sm-8">
               <div class="mb-3">
                 <label for="title" class="form-label">標題</label>
-                <input id="title" type="text" class="form-control" placeholder="請輸入標題" />
+                <input
+                  id="title"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入標題"
+                  v-model="productInfo.title"
+                />
               </div>
 
               <div class="row">
                 <div class="mb-3 col-md-6">
                   <label for="category" class="form-label">分類</label>
-                  <input id="category" type="text" class="form-control" placeholder="請輸入分類" />
+                  <input
+                    id="category"
+                    type="text"
+                    class="form-control"
+                    placeholder="請輸入分類"
+                    v-model="productInfo.category"
+                  />
                 </div>
                 <div class="mb-3 col-md-6">
                   <label for="price" class="form-label">單位</label>
-                  <input id="unit" type="text" class="form-control" placeholder="請輸入單位" />
+                  <input
+                    id="unit"
+                    type="text"
+                    class="form-control"
+                    placeholder="請輸入單位"
+                    v-model="productInfo.unit"
+                  />
                 </div>
               </div>
 
@@ -101,6 +138,7 @@
                     min="0"
                     class="form-control"
                     placeholder="請輸入原價"
+                    v-model="productInfo.origin_price"
                   />
                 </div>
                 <div class="mb-3 col-md-6">
@@ -111,6 +149,7 @@
                     min="0"
                     class="form-control"
                     placeholder="請輸入售價"
+                    v-model="productInfo.price"
                   />
                 </div>
               </div>
@@ -123,6 +162,7 @@
                   type="text"
                   class="form-control"
                   placeholder="請輸入產品描述"
+                  v-model="productInfo.description"
                 >
                 </textarea>
               </div>
@@ -133,6 +173,7 @@
                   type="text"
                   class="form-control"
                   placeholder="請輸入說明內容"
+                  v-model="productInfo.content"
                 >
                 </textarea>
               </div>
@@ -155,11 +196,164 @@
           <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
             取消
           </button>
-          <button type="button" class="btn btn-primary">確認</button>
+
+          <button type="button" class="btn btn-primary" @click="confirmEdit(data, id)">
+            確認修改
+          </button>
         </div>
       </div>
     </div>
   </div>
+  <!-- edit Modal -->
+  <div
+    id="editModal"
+    ref="productModal"
+    class="modal fade"
+    tabindex="-1"
+    aria-labelledby="productModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 id="productModalLabel" class="modal-title">
+            <span>修改產品</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="mb-2">
+                <div class="mb-3">
+                  <label for="imageUrl" class="form-label">輸入圖片網址</label>
+                  <input type="text" class="form-control" placeholder="請輸入圖片連結" />
+                </div>
+                <img class="img-fluid" src="" alt="" />
+              </div>
+              <div>
+                <button class="btn btn-outline-primary btn-sm d-block w-100">新增圖片</button>
+              </div>
+              <div>
+                <button class="btn btn-outline-danger btn-sm d-block w-100">刪除圖片</button>
+              </div>
+            </div>
+            <div class="col-sm-8">
+              <div class="mb-3">
+                <label for="title" class="form-label">標題</label>
+                <input
+                  id="title"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入標題"
+                  v-model="productInfo.title"
+                />
+              </div>
+
+              <div class="row">
+                <div class="mb-3 col-md-6">
+                  <label for="category" class="form-label">分類</label>
+                  <input
+                    id="category"
+                    type="text"
+                    class="form-control"
+                    placeholder="請輸入分類"
+                    v-model="productInfo.category"
+                  />
+                </div>
+                <div class="mb-3 col-md-6">
+                  <label for="price" class="form-label">單位</label>
+                  <input
+                    id="unit"
+                    type="text"
+                    class="form-control"
+                    placeholder="請輸入單位"
+                    v-model="productInfo.unit"
+                  />
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="mb-3 col-md-6">
+                  <label for="origin_price" class="form-label">原價</label>
+                  <input
+                    id="origin_price"
+                    type="number"
+                    min="0"
+                    class="form-control"
+                    placeholder="請輸入原價"
+                    v-model="productInfo.origin_price"
+                  />
+                </div>
+                <div class="mb-3 col-md-6">
+                  <label for="price" class="form-label">售價</label>
+                  <input
+                    id="price"
+                    type="number"
+                    min="0"
+                    class="form-control"
+                    placeholder="請輸入售價"
+                    v-model="productInfo.price"
+                  />
+                </div>
+              </div>
+              <hr />
+
+              <div class="mb-3">
+                <label for="description" class="form-label">產品描述</label>
+                <textarea
+                  id="description"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入產品描述"
+                  v-model="productInfo.description"
+                >
+                </textarea>
+              </div>
+              <div class="mb-3">
+                <label for="content" class="form-label">說明內容</label>
+                <textarea
+                  id="description"
+                  type="text"
+                  class="form-control"
+                  placeholder="請輸入說明內容"
+                  v-model="productInfo.content"
+                >
+                </textarea>
+              </div>
+              <div class="mb-3">
+                <div class="form-check">
+                  <input
+                    id="is_enabled"
+                    class="form-check-input"
+                    type="checkbox"
+                    :true-value="1"
+                    :false-value="0"
+                  />
+                  <label class="form-check-label" for="is_enabled">是否啟用</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+            取消修改
+          </button>
+
+          <button type="button" class="btn btn-primary" @click="confirmEdit(productInfo, id)">
+            確認修改
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- delete Modal -->
   <div
     id="delProductModal"
     ref="delProductModal"
@@ -189,7 +383,7 @@
           <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
             取消
           </button>
-          <button type="button" class="btn btn-danger">確認刪除</button>
+          <button type="button" class="btn btn-danger" @click="delProduct(dataId)">確認刪除</button>
         </div>
       </div>
     </div>
@@ -199,18 +393,27 @@
 <script setup lang="js" name="ProductsAdminView">
 import { ref, reactive, onMounted } from 'vue'
 import axios from 'axios'
+//數據
 const token = ref('')
 const products = reactive([])
-function getProducts() {
-  axios
-    .get('https://ec-course-api.hexschool.io/v2/api/cd131423/admin/products')
-    .then((res) => {
-      products.push(...res.data.products)
-    })
-    .catch((err) => {
-      console.log(err.data)
-    })
-}
+const dataId = ref('')
+const productInfo = reactive({
+  category: '',
+  content: '',
+  description: '',
+  id: '',
+  imageUrl: '',
+  imagesUrl: [],
+  is_enabled: '',
+  num: '',
+  origin_price: '',
+  price: '',
+  title: '',
+  unit: ''
+})
+const id = ref('')
+
+//鉤子
 onMounted(() => {
   token.value = document.cookie.replace(
     /(?:(?:^|.*;\s*)cd131423token\s*\=\s*([^;]*).*$)|^.*$/,
@@ -219,8 +422,10 @@ onMounted(() => {
 
   axios.defaults.headers.common['Authorization'] = token.value
   checkLogin()
-  console.log(token.value)
+  console.log(token)
 })
+
+//方法
 function checkLogin() {
   axios
     .post(`https://vue3-course-api.hexschool.io/v2/api/user/check`)
@@ -229,6 +434,47 @@ function checkLogin() {
     })
     .catch((err) => {
       console.log(err)
+    })
+}
+function getProducts() {
+  axios
+    .get('https://ec-course-api.hexschool.io/v2/api/cd131423/admin/products')
+    .then((res) => {
+      products.push(...res.data.products)
+      //   console.log(products)
+    })
+    .catch((err) => {
+      console.log(err.data)
+    })
+}
+function delProduct(id) {
+  axios
+    .delete(`https://ec-course-api.hexschool.io/v2/api/cd131423/admin/product/${id}`)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err.data)
+    })
+}
+function editProduct(item, idVal) {
+  console.log(item, idVal)
+  Object.assign(productInfo, item)
+  id.value = idVal
+  console.log(productInfo)
+  console.log(id.value)
+}
+function confirmEdit(data, id) {
+  axios
+    .put(
+      `https://ec-course-api.hexschool.io/v2/api/cd131423/admin/product/${id},
+      {"data":${data}}`
+    )
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err.data)
     })
 }
 </script>
